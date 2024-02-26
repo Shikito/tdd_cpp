3b450f2c0dc82e16920370f8aa8d127c9cfef15b
Shikito <shikito.aos@gmail.com>
2024-02-26 11:09:20 +0900

    refactor Dollar class

--- src/Dollar.h
+++ src/Dollar.h
@@ -1,15 +1,14 @@
-#ifndef DOLLAR_H
-#define DOLLAR_H
+#pragma once
 
 class Dollar{
 public:  
     //コンストラクタ
-    explicit Dollar(int initial_amount) : amount(initial_amount){};
+    explicit Dollar(int initial_amount) : amount{initial_amount}{};
     //メンバ関数
-    Dollar times(int multiplier) {return Dollar(amount * multiplier);};
+    Dollar times(int multiplier) const {return Dollar(amount * multiplier);};
     bool operator==(const Dollar &other) const { return amount == other.amount; };
     bool operator!=(const Dollar &other) const { return !(*this == other); };
 private:
+    //メンバ変数
     int amount;
 };
-#endif
--- tests/MoneyTest.cpp
+++ tests/MoneyTest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "Dollar.h"
+#include "Franc.h"
 
 TEST(MoneyTest, TestMultiplication){
     Dollar five(5);
@@ -12,4 +13,12 @@ TEST(MoneyTest, TestMultiplication){
 TEST(MoneyTest, TestEquality){
     ASSERT_EQ(Dollar(5), Dollar(5));
     ASSERT_NE(Dollar(5), Dollar(6));
+}
+
+TEST(MoneyTest, TestFrancMultiplication){
+    Franc five(5);
+    ASSERT_EQ(Franc(10), five.times(2));
+
+    // check five doesn't change.;
+    ASSERT_EQ(Franc(15), five.times(3));
 }
