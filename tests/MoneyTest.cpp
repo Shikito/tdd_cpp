0e943f4b940f10d7ef154030af87d1051ed7cc87
Shikito <shikito.aos@gmail.com>
2024-02-26 09:31:43 +0900

    add comment

--- tests/MoneyTest.cpp
+++ tests/MoneyTest.cpp
@@ -5,6 +5,8 @@ TEST(MoneyTest, TestMultiplication){
     Dollar five(5);
     Dollar product = five.times(2);
     ASSERT_EQ(10, product.amount);
+
+    // check five doesn't change.
     product = five.times(3);
     ASSERT_EQ(15, product.amount);
 }
